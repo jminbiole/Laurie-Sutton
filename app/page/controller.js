80f8087647ddbbacf60aa35f6793e4a663e70cb6
Jeff Minbiole <minbiole.jeff@gmail.com>
2016-10-23 02:58:21 -0500

    simple layout esign, still testing

---
+++ app/page/controller.js
@@ -0,0 +1,45 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  actions: {
+      classNames: ['back-to-top'],
+      tagName: 'footer',
+      time: 500,
+
+      didInsertElement() {
+        Ember.$(window).scroll(() => {
+          if (Ember.$(window).scrollTop() > Ember.$(window).height() / 2) {
+            this.$().fadeIn(this.time);
+          } else {
+            this.$().fadeOut(this.time);
+          }
+        });
+      },
+
+      toTop() {
+        Ember.$(`html, body`).animate({ scrollTop: 0 }, this.time);
+      },
+      scrollToRecent() {
+   Ember.$(`html, body`).animate({
+     scrollTop: Ember.$(`#recent`).offset().top,
+   }, this.time);
+ },
+      scrollToAbout() {
+   Ember.$(`html, body`).animate({
+     scrollTop: Ember.$(`#about`).offset().top,
+   }, this.time);
+ },
+
+ scrollToContact() {
+   Ember.$(`html, body`).animate({
+     scrollTop: Ember.$(`#contact`).offset().top,
+   }, this.time);
+ },
+
+ scrollToProjects() {
+   Ember.$(`html, body`).animate({
+     scrollTop: Ember.$(`#projects`).offset().top,
+   }, this.time);
+ },
+  }
+});
--- app/router.js
+++ app/router.js
@@ -7,7 +7,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('landing');
+  this.route('page', {path: '/'});
 });
 
 export default Router;
